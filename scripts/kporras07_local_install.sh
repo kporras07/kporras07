d7ac653c716c0de5b3a0edeb505067c08a81bf07
Kevin Porras <kporras07@gmail.com>
2017-01-05 15:23:59 -0600

    Make install script work again.

--- scripts/kporras07_local_install.sh
+++ scripts/kporras07_local_install.sh
@@ -2,9 +2,12 @@
 if [ -f ./.vendor/bin/drush ] ; then mv ./.vendor ./vendor ; fi
 DRUSH="./vendor/bin/drush"
 SITE_ALIAS="@kporras07.kporras07.dev"
+SITE_UUID="16219eb2-a290-46ad-a8ab-fb8e34672ae7"
 $DRUSH $SITE_ALIAS cc drush
 echo "Installing..."
-if [ -f ./files/config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS si standard --config-dir=sites/default/files/config/sync --account-pass=admin -y ; else $DRUSH $SITE_ALIAS si standard --account-pass=admin -y ; fi
+if [ -f ./files/config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS si kporras07 --account-pass=admin -y ; else $DRUSH $SITE_ALIAS si kporras07 --account-pass=admin -y ; fi
+echo "Set site uuid..."
+$DRUSH $SITE_ALIAS config-set "system.site" uuid "$SITE_UUID" -y
 echo "Importing config..."
 if [ -f ./files/config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS cim -y ; fi
 echo "Cleaning cache..."
